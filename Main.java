22c69be54bd52739b25a30d46871678a288b6ceb
bibinwithab <bibingraceson@gmail.com>
2023-07-30 22:03:41 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class Main{
 	public static void main(String[] args){
-		System.out.println("Helllo, world");
+		
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter name: ");
+
+		String name = input.nextLine();
+
+		System.out.println(name + " is a nigger");
+
 	}
 }
